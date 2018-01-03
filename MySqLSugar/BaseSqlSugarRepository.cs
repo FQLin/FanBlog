7d9d73dd1bb49deb33ee09d7859d57dc6a50af12
FQLin <fanqinglin1990@hotmail.com>
2018-01-03 13:03:53 +0800

    MySqlSugar

---
+++ MySqLSugar/BaseSqlSugarRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Options;
+using SqlSugar;
+
+namespace FanBlog.Data.MySqLSugar
+{
+    public abstract class BaseSqlSugarRepository
+    {
+        protected readonly SqlSugarClient _db;
+
+        protected BaseSqlSugarRepository(SqlSugarClient client)
+        {
+            this._db = client;
+        }
+    }
+}
