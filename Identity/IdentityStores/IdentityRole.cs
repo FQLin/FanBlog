87a33abb7128e2184dcf2cd796f675f940049ea2
FQLin <fanqinglin1990@hotmail.com>
2018-02-02 16:37:38 +0800

    Add files via upload

---
+++ Identity/IdentityStores/IdentityRole.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FanBlog.IdentityStores
+{
+    /// <summary>
+    /// Represents a role in the identity system
+    /// </summary>
+    /// <typeparam name="TKey">The type used for the primary key for the role.</typeparam>
+    public class IdentityRole<TKey> where TKey : IEquatable<TKey>
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="IdentityRole{TKey}"/>.
+        /// </summary>
+        public IdentityRole() { }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="IdentityRole{TKey}"/>.
+        /// </summary>
+        /// <param name="roleName">The role name.</param>
+        public IdentityRole(string roleName) : this()
+        {
+            Name = roleName;
+        }
+
+        /// <summary>
+        /// Gets or sets the primary key for this role.
+        /// </summary>
+        public virtual TKey Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name for this role.
+        /// </summary>
+        public virtual string Name { get; set; }
+
+        /// <summary>
+        /// Returns the name of the role.
+        /// </summary>
+        /// <returns>The name of the role.</returns>
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Identity/IdentityStores/IdentityUser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FanBlog.IdentityStores
+{
+    /// <summary>
+    /// Represents a user in the identity system
+    /// </summary>
+    /// <typeparam name="TKey">The type used for the primary key for the user.</typeparam>
+    public class IdentityUser<TKey> where TKey : IEquatable<TKey>
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="IdentityUser{TKey}"/>.
+        /// </summary>
+        public IdentityUser() { }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="IdentityUser{TKey}"/>.
+        /// </summary>
+        /// <param name="userName">The user name.</param>
+        public IdentityUser(string userName) : this()
+        {
+            UserName = userName;
+        }
+
+        /// <summary>
+        /// Gets or sets the primary key for this user.
+        /// </summary>
+        public virtual TKey Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user name for this user.
+        /// </summary>
+        public virtual string UserName { get; set; }
+        
+        /// <summary>
+        /// Gets or sets a salted and hashed representation of the password for this user.
+        /// </summary>
+        public virtual string Password { get; set; }
+
+        public virtual string Name { get; set; }
+        
+        /// <summary>
+        /// Returns the username for this user.
+        /// </summary>
+        public override string ToString()
+        {
+            return UserName;
+        }
+    }
+}
